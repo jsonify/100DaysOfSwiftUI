4a83760e1d0ddead339b99f9f2396dcd27a008be
Jason <phlacin@gmail.com>
2019-10-11 20:03:44 -0700

    Starting P01

---
+++ P01 WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Jason Rueckert on 10/11/19.
+//  Copyright © 2019 Jason Rueckert. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
