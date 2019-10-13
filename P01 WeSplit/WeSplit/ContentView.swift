c32be36e89680bfc59e7d38712e695979d9c9176
Jason <phlacin@gmail.com>
2019-10-12 22:01:49 -0700

    Finished overview of State, 2-way binding, ForEach loop

--- P01 WeSplit/WeSplit/ContentView.swift
+++ P01 WeSplit/WeSplit/ContentView.swift
@@ -9,15 +9,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    let students = ["Harry", "Hermoine", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
     var body: some View {
-        NavigationView {
-            Form {
-                Section {
-                    Text("Hello World")
-                }
+        Picker("Select your student", selection: $selectedStudent) {
+            ForEach(0..<students.count) {
+                Text(self.students[$0])
             }
-            .navigationBarTitle(Text("SwiftUI"))
         }
+        
     }
 }
 
