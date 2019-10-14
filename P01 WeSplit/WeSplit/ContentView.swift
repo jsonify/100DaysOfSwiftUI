48bc1fe546febea35a796ff00573d2f12b1d4d32
Jason <phlacin@gmail.com>
2019-10-14 07:56:08 -0700

    Getting percentage to look right with header

--- P01 WeSplit/WeSplit/ContentView.swift
+++ P01 WeSplit/WeSplit/ContentView.swift
@@ -9,16 +9,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Harry", "Hermoine", "Ron"]
-    @State private var selectedStudent = "Harry"
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
-        Picker("Select your student", selection: $selectedStudent) {
-            ForEach(0..<students.count) {
-                Text(self.students[$0])
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section(header: Text("How much tip do you want to leave?")) {
+                    
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0..<tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                .pickerStyle(SegmentedPickerStyle())
+                }
+                Section {
+                    Text("$\(checkAmount)")
+                }
             }
+            .navigationBarTitle("WeSplit")
         }
-        
     }
 }
 
