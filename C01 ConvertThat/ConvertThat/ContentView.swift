af9cc58e981a93acb0310cb16274a6d15ea15dc7
Jason <phlacin@gmail.com>
2019-10-14 22:08:27 -0700

    Conversion is hard

--- C01 ConvertThat/ConvertThat/ContentView.swift
+++ C01 ConvertThat/ConvertThat/ContentView.swift
@@ -10,55 +10,63 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var unitInput = ""
-    @State private var m = 0
+    @State private var m = 0.0
+    @State private var km = 0.0
+    @State private var ft = 0.0
+    @State private var yard = 0.0
+    @State private var mile = 0.0
+    
     @State private var cm = 0
     @State private var mm = 0
-    @State private var yard = 0
-    @State private var ft = 0
     @State private var inch = 0
-    @State private var km = 0
-    @State private var mile = 0
     
-    @State private var unitType = 0
+    @State private var unitInputType = 0
+    @State private var unitOutputType = 0
     
-    let unitTypes = ["Meters", "Km", "Ft", "Yards", "Miles"]
-//    func convertAmount(amount: Double)
+    let unitInputTypes = ["Meters", "Km", "Ft", "Yards", "Miles"]
+    let unitOutputTypes = ["Meters", "Km", "Ft", "Yards", "Miles"]
     
-    var convertToMM: Double {
-        
+//    func convertAmountMM(amount: ) {
+//        if(unitInputType == 0) {
+//            km = amount * 1000
+//            ft = amount * 3.28084
+//            yard = amount * 1.093613
+//            mile = amount * 0.000621
+//        }
+//    }
+    
+    var convertedAmount: Double {
+        let quantity = Double(unitInput)
         return 0
     }
     
     var body: some View {
         NavigationView {
             Form {
-                Section(header: Text("Input quantity")) {
+                Section(header: Text("How many?")) {
                     TextField("Enter quantity", text: $unitInput)
                         .keyboardType(.decimalPad)
                 }
-                Section(header: Text("Unit Type")) {
-                    Picker("Unit Type", selection: $unitType) {
-                        ForEach(0..<unitTypes.count) {
-                            Text("\(self.unitTypes[$0])")
+                Section(header: Text("Input Unit Type")) {
+                    Picker("Unit Type", selection: $unitInputType) {
+                        ForEach(0..<unitInputTypes.count) {
+                            Text("\(self.unitInputTypes[$0])")
                         }
                     }
                     .pickerStyle(SegmentedPickerStyle())
                 }
-                Section(header: Text("Meters")) {
-                    Text("\(m) m")
+                Section(header: Text("Output Unit Type")) {
+                    Picker("Unit Type", selection: $unitOutputType) {
+                        ForEach(0..<unitOutputTypes.count) {
+                            Text("\(self.unitOutputTypes[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
                 }
-                Section(header: Text("Kilometers")) {
-                    Text("\(km) km")
+                Section(header: Text("Converted Length")) {
+                    Text("\(convertedAmount, specifier: "%.3f") units")
                 }
-                Section(header: Text("Feet")) {
-                    Text("\(ft) ft")
-                }
-                Section(header: Text("Yards")) {
-                    Text("\(yard) y")
-                }
-                Section(header: Text("Miles")) {
-                    Text("\(mile) miles")
-                }
+
                 
             }
             .navigationBarTitle("Length Converter")
