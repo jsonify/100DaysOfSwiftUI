028d752861974a6410ce8dae68390995d6a075cb
Jason <phlacin@gmail.com>
2019-10-17 16:14:13 -0700

    Finished C01

--- C01 ConvertThat/ConvertThat/ContentView.swift
+++ C01 ConvertThat/ConvertThat/ContentView.swift
@@ -9,67 +9,92 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var unitInput = ""
-    @State private var m = 1.0
-    @State private var km = 1000.0
-    @State private var ft = 3.28084
-    @State private var yard = 1.093613
-    @State private var mile = 0.000621
     
-    @State private var cm = 0
-    @State private var mm = 0
-    @State private var inch = 0
+    //Give the user input TextField a default of 1, & set the segmentedPickers to the middle selection
+    @State private var unitInput = "1"
+    @State private var unitInputType = 3
+    @State private var unitOutputType = 3
     
-    @State private var unitInputType = 0
-    @State private var unitOutputType = 0
+    //An array defines the choices for the segmentedPicker
+    var lengthUnits = ["mm", "cm", "m", "km", "inch", "feet", "yard", "mile"]
+
     
-    let unitInputTypes = ["Km", "Ft", "Yards", "Miles"]
-    
-    let unitOutputTypes = ["Meters", "Km", "Ft", "Yards", "Miles"]
-    
+    //The calculated property below converts the input quantity & unit to mm and then to the desired conversion unit
     var convertedOutputType: Double {
-        let amount = Double(unitInput) ?? 0
-        return amount
+        let startUnit = lengthUnits[unitInputType]
+        var startFactor: Double = 1
+        
+        let endUnit = lengthUnits[unitOutputType]
+        var endFactor: Double = 1
+        
+        switch startUnit {
+        case "cm":
+            startFactor = 10
+        case "m":
+            startFactor = 100
+        case "km":
+            startFactor = 1000
+        case "inch":
+            startFactor = 25.4
+        case "feet":
+            startFactor = 25.4 * 12
+        case "yard":
+            startFactor = 25.4 * 12 * 3
+        case "mile":
+            startFactor = 25.4 * 12 * 5280
+        default:
+            startFactor = 1
+        }
+        
+        let mmConversion = startFactor * Double(Double(unitInput) ?? 0)
+        
+        switch endUnit {
+        case "cm":
+            endFactor = 1/10
+        case "m":
+            endFactor = 1/100
+        case "km":
+            endFactor = 1/1000
+        case "inch":
+            endFactor = 1/25.4
+        case "feet":
+            endFactor = 1/(25.4 * 12)
+        case "yard":
+            endFactor = 1/(25.4 * 12 * 3)
+        case "mile":
+            endFactor = 1/(25.4 * 12 * 5280)
+        default:
+            endFactor = 1
+        }
+        
+        return mmConversion * endFactor
     }
-    
-    // take a quantity and convert it to a double
-    var convertedQuantity: Double {
-        return Double(unitInput) ?? 0
-    }
-    
-    /*
-     
-     take the current input segment and establish it as the base
-     take the current output segment and establish it as the multiplier
-     create a variable that is the base times multiplier
-     show variable output
-     */
-    
+
     var body: some View {
         NavigationView {
             Form {
-                Section(header: Text("How many?")) {
-                    TextField("Enter quantity", text: $unitInput)
-                        .keyboardType(.decimalPad)
-                }
                 Section(header: Text("Input Unit Type")) {
                     Picker("Unit Type", selection: $unitInputType) {
-                        ForEach(0..<unitInputTypes.count) {
-                            Text("\(self.unitInputTypes[$0])")
+                        ForEach(0 ..< lengthUnits.count) {
+                            Text("\(self.lengthUnits[$0])")
                         }
                     }
                     .pickerStyle(SegmentedPickerStyle())
                 }
+                Section(header: Text("How many?")) {
+                    TextField("Enter quantity", text: $unitInput)
+                        .keyboardType(.decimalPad)
+                }
                 Section(header: Text("Output Unit Type")) {
                     Picker("Unit Type", selection: $unitOutputType) {
-                        ForEach(0..<unitOutputTypes.count) {
-                            Text("\(self.unitOutputTypes[$0])")
+                        ForEach(0..<lengthUnits.count) {
+                            Text("\(self.lengthUnits[$0])")
                         }
                     }
                     .pickerStyle(SegmentedPickerStyle())
                 }
                 Section(header: Text("Converted Length")) {
-                    Text("\(convertedQuantity, specifier: "%.3f") meters")
+                    Text("\(convertedOutputType, specifier: "%.5f")")
                 }
 
                 
