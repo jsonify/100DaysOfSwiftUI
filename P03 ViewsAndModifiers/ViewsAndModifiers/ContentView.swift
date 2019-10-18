b3508b8b5b4e0de0e10afec5ce815291956348d6
Jason <phlacin@gmail.com>
2019-10-17 22:08:01 -0700

    Views and Modifiers

--- P03 ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ P03 ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.red)
-            .edgesIgnoringSafeArea(.all)
+        Button("Hello World") {
+            print(type(of: self.body))
+        }
+            .frame(width: 200, height: 200)
+        .background(Color.red)
     }
 }
 
