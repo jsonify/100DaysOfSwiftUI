b82daddc211efc377f30ed60143c9e8505d05285
Jason <phlacin@gmail.com>
2019-10-21 06:38:42 -0700

    Started P04 and basics

---
+++ P04 BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Jason Rueckert on 10/21/19.
+//  Copyright © 2019 Jason Rueckert. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    
+    var body: some View {
+        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+            Text("\(sleepAmount, specifier: "%g") hours")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
