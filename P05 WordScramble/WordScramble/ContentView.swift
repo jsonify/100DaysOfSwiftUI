47660a24516167d5589423993aab5aa857fbc503
Jason Rueckert <phlacin@gmail.com>
2019-10-31 16:12:00 -0700

    Started p05 and basics

---
+++ P05 WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Jason on 10/31/19.
+//  Copyright © 2019 Jason. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
