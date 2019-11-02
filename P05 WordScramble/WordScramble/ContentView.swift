4b1c297d719a3ddaff93fbd04ca707fce77c5ec6
Jason Rueckert <phlacin@gmail.com>
2019-11-01 19:50:24 -0700

    Working on setting up Lists

--- P05 WordScramble/WordScramble/ContentView.swift
+++ P05 WordScramble/WordScramble/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        List(0..<5) {
+            Text("Dynamic Row \($0)")
+        }
+        .listStyle(GroupedListStyle())
     }
 }
 
