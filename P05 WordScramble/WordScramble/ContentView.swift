d28a381b83e26025cd59a30565fbef85d81219f9
Jason Rueckert <phlacin@gmail.com>
2019-11-03 06:48:31 -0800

    Got word list prepped with character count symbols on left

--- P05 WordScramble/WordScramble/ContentView.swift
+++ P05 WordScramble/WordScramble/ContentView.swift
@@ -9,13 +9,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    
     var body: some View {
-        let word = "Swift"
-        let checker = UITextChecker()
-        let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        let allGood = misspelledRange.location == NSNotFound
-        return Text("Hell World")
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocapitalization(.none)
+                    .padding()
+                
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+            }
+            .navigationBarTitle(rootWord)
+        }
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else {
+            return
+        }
+        //extra validation later
+        usedWords.insert(answer, at: 0)
+        newWord = ""
     }
 }
 
