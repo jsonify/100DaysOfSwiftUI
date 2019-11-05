c956d2110cc5a3209df8d75bfaad78dd7398c1bb
Jason Rueckert <phlacin@gmail.com>
2019-11-05 07:25:38 -0800

    Explicit animations

--- P06 Animations/Animations/ContentView.swift
+++ P06 Animations/Animations/ContentView.swift
@@ -9,25 +9,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount: CGFloat = 1
+    @State private var animationAmount = 0.0
+    
     
     var body: some View {
-        print(animationAmount)
-        
-        return VStack {
-            Stepper("Scale Amount", value: $animationAmount.animation(Animation.easeIn(duration: 1)
-                .repeatCount(3, autoreverses: true)
-            ), in: 1...10)
-            Spacer()
-            Button("Tap Me") {
-                self.animationAmount += 1
+        Button("Hello") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                self.animationAmount += 360
             }
-            .padding(40)
-            .foregroundColor(.white)
-            .background(Color.red)
-        .clipShape(Circle())
-        .scaleEffect(animationAmount)
         }
+        .padding(50)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+        
     }
 }
 
