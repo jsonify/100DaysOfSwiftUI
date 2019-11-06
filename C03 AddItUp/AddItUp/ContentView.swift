c9d6c4908a63c480b6c3349e10fc7a656fe6f73e
Jason Rueckert <phlacin@gmail.com>
2019-11-05 16:14:33 -0800

    Basic layout almost complete

--- C03 AddItUp/AddItUp/ContentView.swift
+++ C03 AddItUp/AddItUp/ContentView.swift
@@ -9,8 +9,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var secondNumber = 0
+    @State private var maxNumber = 5
+    @State private var questionInputCount = 2
+    @State private var question = 1
+    @State private var answer = ""
+    
+//    @State private var firstNumber = 0
+    var firstNumber: Int {
+        return 0
+    }
+    var numberOfQuestions = [5, 10, 15, 20]
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Form {
+                Section(header: Text("Select your ADDITION table")) {
+                    Stepper(value: $maxNumber, in: 0...10, step: 1) {
+                        HStack {
+                            Text("Up to:")
+                            Text("\(maxNumber)")
+                        }
+                    }
+                }
+                Section(header: Text("How many questions")) {
+                    Picker("Questions", selection: $questionInputCount) {
+                        ForEach(0 ..< numberOfQuestions.count) {
+                            Text("\(self.numberOfQuestions[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Question \(question)")) {
+                    HStack {
+                        Spacer()
+                        Text("\(firstNumber)")
+                            .font(.largeTitle)
+                        Spacer()
+                        Text("+")
+                        .font(.title)
+                        Spacer()
+                        Text("\(firstNumber)")
+                        .font(.largeTitle)
+                        Spacer()
+                    }
+                }
+                Section(header: Text("Your answer:")) {
+                    TextField("Enter quantity", text: $answer)
+                        .keyboardType(.decimalPad)
+                }
+            }
+        .navigationBarTitle("Add It Up!")
+        }
     }
 }
 
