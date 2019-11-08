7eaa35bc500125e051f6c35a46c7ebbefa32c48a
Jason Rueckert <phlacin@gmail.com>
2019-11-08 08:40:14 -0800

    Setup P08 - Resizing with GeometryReader

---
+++ P08 Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Jason on 11/8/19.
+//  Copyright © 2019 Jason. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                Section(header: Text("Basics")) {
+                    NavigationLink("Resizing images to fit the screen using GeometryReader", destination: UsingGeometryReader())
+                }
+                
+                Section(header: Text("Main")) {
+                    NavigationLink("Moonshot Application", destination: Application())
+                }
+            }
+        .navigationBarTitle("P08 - Moonshot")
+        }
+        .environment(\.colorScheme, .dark)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environment(\.colorScheme, .dark)
+    }
+}
---
+++ P08 Moonshot/Moonshot/Main/Application.swift
@@ -0,0 +1,21 @@
+//
+//  Application.swift
+//  Moonshot
+//
+//  Created by Jason on 11/8/19.
+//  Copyright © 2019 Jason. All rights reserved.
+//
+
+import SwiftUI
+
+struct Application: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Application_Previews: PreviewProvider {
+    static var previews: some View {
+        Application()
+    }
+}
