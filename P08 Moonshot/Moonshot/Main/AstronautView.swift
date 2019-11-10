00b5195ebf259e30e44ed38253e3d7adca1db935
Jason Rueckert <phlacin@gmail.com>
2019-11-09 21:20:22 -0800

    Finished P08 - C1 and C2

--- P08 Moonshot/Moonshot/Main/AstronautView.swift
+++ P08 Moonshot/Moonshot/Main/AstronautView.swift
@@ -10,30 +10,51 @@ import SwiftUI
 
 struct AstronautView: View {
     let astronaut: Astronaut
+    let missions: [Mission]
     
     var body: some View {
         GeometryReader { geometry in
             VStack {
                 ScrollView(.vertical) {
-                Image(self.astronaut.id)
-                .resizable()
-                .scaledToFit()
-                    .frame(width: geometry.size.width)
-                
-                Text(self.astronaut.description)
-                .padding()
-                    .layoutPriority(1)
-                
+                    Image(self.astronaut.id)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geometry.size.width)
+                    
+                    Text(self.astronaut.description)
+                        .padding()
+                        .layoutPriority(1)
+                    
+//                    Challenge 2
+                    List {
+                        Section(header: Text("Missions")) {
+                            ForEach(self.missions) { mission in
+                                Text(mission.displayName)
+                            }
+                        }
+                    }
+                }
             }
         }
-        }
         .navigationBarTitle(Text(astronaut.name), displayMode: .inline)
+    }
+    
+    init(astronaut: Astronaut, mission: [Mission]) {
+        self.astronaut = astronaut
+        
+        self.missions = mission.filter({ mission in
+            mission.crew.contains(where: {
+                $0.name == astronaut.id
+            })
+        })
     }
 }
 
 struct AstronautView_Previews: PreviewProvider {
     static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+    
     static var previews: some View {
-        AstronautView(astronaut: astronauts[0])
+        AstronautView(astronaut: astronauts[0], mission: missions)
     }
 }
--- P08 Moonshot/Moonshot/Main/MissionView.swift
+++ P08 Moonshot/Moonshot/Main/MissionView.swift
@@ -16,6 +16,7 @@ struct MissionView: View {
     
     let mission: Mission
     let astronauts: [CrewMember]
+    let missions: [Mission]
     
     var body: some View {
         GeometryReader { geometry in
@@ -27,11 +28,14 @@ struct MissionView: View {
                         .frame(maxWidth: geometry.size.width * 0.7)
                         .padding(.top)
                     
+                    // Challenge 1
+                    Text("Launch Date: \(self.mission.formattedLaunchDate)")
+                    
                     Text(self.mission.description)
                         .padding()
                     
                     ForEach(self.astronauts, id: \.role) { crewMember in
-                        NavigationLink(destination: AstronautView(astronaut: crewMember.astronaut)) {
+                        NavigationLink(destination: AstronautView(astronaut: crewMember.astronaut, mission: self.missions)) {
                             HStack {
                                 Image(crewMember.astronaut.id)
                                     .resizable()
@@ -59,7 +63,7 @@ struct MissionView: View {
         .navigationBarTitle(Text(mission.displayName), displayMode: .inline)
     }
     
-    init(mission: Mission, astronauts: [Astronaut]) {
+    init(mission: Mission, astronauts: [Astronaut], missions: [Mission]) {
         self.mission = mission
         
         var matches = [CrewMember]()
@@ -73,6 +77,7 @@ struct MissionView: View {
         }
         
         self.astronauts = matches
+        self.missions = missions
     }
     
 }
@@ -82,7 +87,7 @@ struct MissionView_Previews: PreviewProvider {
     
     static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
     static var previews: some View {
-        MissionView(mission: missions[0], astronauts: astronauts)
+        MissionView(mission: missions[0], astronauts: astronauts, missions: missions)
         
     }
 }
