927297419f201c0901b6b2a245fb21328382657a
Jason Rueckert <phlacin@gmail.com>
2019-11-11 21:33:29 -0800

    Added TabView for multiple views

--- P09 Drawing/Drawing/Fundamentals/UsingImagePaint.swift
+++ P09 Drawing/Drawing/Fundamentals/UsingImagePaint.swift
@@ -7,7 +7,7 @@
 //
 /*
  SwiftUI gives us a dedicated type that wraps images in a way that we have complete control over how they should be rendered, which in turn means we can use them for borders and fills without problem.
-
+ 
  The type is called ImagePaint, and it’s created using one to three parameters. At the very least you need to give it an Image to work with as its first parameter, but you can also provide a rectangle within that image to use as the source of your drawing specified in the range of 0 to 1 (the second parameter), and a scale for that image (the third parameter). Those second and third parameters have sensible default values of “the whole image” and “100% scale”, so you can sometimes ignore them.
  
  ImagePaint will automatically keep tiling its image until it has filled its area – it can work with backgrounds, strokes, borders, and fills of any size.
@@ -16,15 +16,30 @@
 import SwiftUI
 
 struct UsingImagePaint: View {
+    @State private var selection = 2
+    
     var body: some View {
-        Capsule()
-            .strokeBorder(ImagePaint(image: Image("Example"), scale: 0.1), lineWidth: 20)
-            .frame(width: 300, height: 200)
-//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-//            .frame(width: 300, height: 300)
-////            .background(Color.red)
-////            .border(Color.red, width: 30)
-//            .border(ImagePaint(image: Image("Example"), sourceRect:CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), width: 30)
+        TabView(selection: $selection) {
+            Capsule()
+                .strokeBorder(ImagePaint(image: Image("Example"), scale: 0.1), lineWidth: 20)
+                .frame(width: 300, height: 200)
+                .tabItem {
+                    Image(systemName: "1.square.fill")
+                    Text("Capsule")
+            }
+            .tag(0)
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .frame(width: 300, height: 300)
+//                .background(Color.red)
+//                .border(Color.red, width: 30)
+                .border(ImagePaint(image: Image("Example"), sourceRect:CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), width: 30)
+                .tabItem {
+                    Image(systemName: "2.square.fill")
+                    Text("Box")
+            }
+            .tag(1)
+        }
+        .font(.headline)
     }
 }
 
