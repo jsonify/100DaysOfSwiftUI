36f8249483f42d8948ab128824886f8b58b152a6
Jason Rueckert <phlacin@gmail.com>
2019-11-19 22:39:47 -0800

    Creating a custom component with @Binding

--- P11 Bookworm/Bookworm/Fundamentals/CustomComponent.swift
+++ P11 Bookworm/Bookworm/Fundamentals/CustomComponent.swift
@@ -8,9 +8,36 @@
 
 import SwiftUI
 
-struct CustomComponent: View {
+// create a new kind of button: one that stays down when pressed.
+
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColores = [Color(white: 0.6), Color(white: 0.4)]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(title) {
+            self.isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColores), startPoint: .top, endPoint: .bottom))
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+    }
+}
+
+struct CustomComponent: View {
+    @State private var rememberMe = false
+    
+    var body: some View {
+        VStack {
+            //  we’re passing in the binding itself, not the Boolean inside it.
+            PushButton(title: "Remember Me", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
     }
 }
 
