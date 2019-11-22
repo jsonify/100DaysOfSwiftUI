cb25fcc887f6bf2aea218d873ede5eed2cba6233
Jason Rueckert <phlacin@gmail.com>
2019-11-22 12:13:49 -0800

    Showing book details

---
+++ P11 Bookworm/Bookworm/Main/DetailView.swift
@@ -0,0 +1,64 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Jason on 11/22/19.
+//  Copyright © 2019 Jason. All rights reserved.
+//
+
+import SwiftUI
+import CoreData
+
+struct DetailView: View {
+    let book: Book
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                ZStack(alignment: .bottomTrailing) {
+                    Image(self.book.genre ?? "Fantasy")
+                        .frame(maxWidth: geometry.size.width)
+                    
+                    Text(self.book.genre?.uppercased() ?? "FANTASY")
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundColor(.white)
+                    .background(Color.black.opacity(0.75))
+                    .clipShape(Capsule())
+                    .offset(x: -5, y: -5)
+                }
+                
+                Text(self.book.author ?? "Unknown Author")
+                    .font(.title)
+                    .foregroundColor(.secondary)
+                
+                Text(self.book.review ?? "No Review")
+                .padding()
+                
+                RatingView(rating: .constant(Int(self.book.rating)))
+                    .font(.largeTitle)
+                
+                Spacer()
+            }
+        }
+        .navigationBarTitle(Text(book.title ?? "Unknown Book"), displayMode: .inline)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+
+    static var previews: some View {
+        let book = Book(context: moc)
+        book.title = "Test Book"
+        book.author = "Test Author"
+        book.genre = "Fantasy"
+        book.rating = 4
+        book.review = "This was a great book. Really enjoyed it."
+        
+        return NavigationView {
+            DetailView(book: book)
+        }
+    }
+}
