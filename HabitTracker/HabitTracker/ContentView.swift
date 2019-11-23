d2145bcb9b340d5b480ad4c91a8acb16ead2296f
Jason Rueckert <phlacin@gmail.com>
2019-11-22 22:47:44 -0800

    Setup C04 HabitTracker

---
+++ HabitTracker/HabitTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HabitTracker
+//
+//  Created by Jason on 11/22/19.
+//  Copyright © 2019 Jason. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
