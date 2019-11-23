ed2d1b47f07c4ecfe7a061a2c2b15ef2bc0ee59b
Jason Rueckert <phlacin@gmail.com>
2019-11-22 22:32:51 -0800

    Setup P12

---
+++ CoreDataProject/CoreDataProject/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  CoreDataProject
+//
+//  Created by Jason on 11/22/19.
+//  Copyright © 2019 Jason. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Fundamentals")) {
+                    NavigationLink("Why does .self work for ForEach?", destination: UnderstandingForEach())
+                    
+                }
+                
+                Section(header: Text("Main")) {
+                    NavigationLink("Core Data Application", destination: Application())
+                }
+            }
+            .navigationBarTitle("P12 - Core Data Project")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreDataProject/CoreDataProject/Fundamentals/UnderstandingForEach.swift
@@ -0,0 +1,21 @@
+//
+//  UnderstandingForEach.swift
+//  CoreDataProject
+//
+//  Created by Jason on 11/22/19.
+//  Copyright © 2019 Jason. All rights reserved.
+//
+
+import SwiftUI
+
+struct UnderstandingForEach: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UnderstandingForEach_Previews: PreviewProvider {
+    static var previews: some View {
+        UnderstandingForEach()
+    }
+}
