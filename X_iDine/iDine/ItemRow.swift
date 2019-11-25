35914990af7ca039e2eddc3a93b9a48750c39f61
Jason Rueckert <phlacin@gmail.com>
2019-11-25 15:53:17 -0800

    Finished item styling

--- X_iDine/iDine/ItemRow.swift
+++ X_iDine/iDine/ItemRow.swift
@@ -14,10 +14,14 @@ struct ItemRow: View {
     var body: some View {
         HStack {
             Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.gray, lineWidth: 2))
             
             VStack(alignment: .leading) {
                 Text(item.name)
+                    .font(.headline)
                 Text("$\(item.price)")
+                
             }
         }
     }
