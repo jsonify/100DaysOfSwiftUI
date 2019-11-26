5299e321e2163fae7a00ed33867ce729f0cfb684
Jason Rueckert <phlacin@gmail.com>
2019-11-26 09:56:02 -0800

    Added SegmentedControl for Tip

--- X_iDine/iDine/CheckoutView.swift
+++ X_iDine/iDine/CheckoutView.swift
@@ -9,13 +9,59 @@
 import SwiftUI
 
 struct CheckoutView: View {
+    @EnvironmentObject var order: Order
+    
+    static let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    static let tipAmounts = [10, 15, 20, 25, 0]
+    
+    
+    @State private var paymentType = 1
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    @State private var tipAmount = 1
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section {
+                Picker("How do you want to pay?", selection: $paymentType) {
+                    ForEach(0..<Self.paymentTypes.count) {
+                        Text(Self.paymentTypes[$0])
+                    }
+                }
+                
+                Toggle(isOn: $addLoyaltyDetails.animation()) {
+                    Text("Use iDone loyalty card")
+                }
+                
+                if addLoyaltyDetails {
+                    TextField("Enter your iDine id", text:  $loyaltyNumber)
+                }
+            }
+            
+            Section(header: Text("Add a tip?")) {
+                Picker("Percentage amount", selection: $tipAmount) {
+                    ForEach(0..<Self.tipAmounts.count) {
+                        Text("\(Self.tipAmounts[$0])%")
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+            }
+            
+            Section(header:
+                Text("$100")
+            ) {
+                Button("Confirm Order") {
+                    // place order
+                }
+            }
+        }
+        .navigationBarTitle(Text("Payment"), displayMode: .inline)
     }
 }
 
 struct CheckoutView_Previews: PreviewProvider {
+    static let order = Order()
+    
     static var previews: some View {
-        CheckoutView()
+        CheckoutView().environmentObject(order)
     }
 }
