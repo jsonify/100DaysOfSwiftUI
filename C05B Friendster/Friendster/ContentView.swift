29d6ad6f351e15d23bd1bf3d451c09b3536adce1
Jason Rueckert <phlacin@gmail.com>
2019-12-02 12:22:34 -0800

    Trying C05 again

---
+++ C05B Friendster/Friendster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Friendster
+//
+//  Created by Jason on 12/2/19.
+//  Copyright © 2019 Jason. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
