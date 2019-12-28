6810328df4dd25fcf2a79f5a30eb0ab0e19bccaf
Jason Rueckert <phlacin@gmail.com>
2019-12-27 21:29:11 -0800

    Creating tabs with TabView and tabItem()

--- 16 HotProspects/HotProspects/Fundamentals/CreatingTabs.swift
+++ 16 HotProspects/HotProspects/Fundamentals/CreatingTabs.swift
@@ -6,11 +6,38 @@
 //  Copyright © 2019 Jason. All rights reserved.
 //
 
+/*
+ SwiftUI also allows us to control the current view programmatically using state. This takes four steps:
+ */
+
 import SwiftUI
 
 struct CreatingTabs: View {
+    // 1. we need some state to track the current tab
+    @State private var selectedTab = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // 3. we need to bind the selection of the TabView to $selectedTab
+        TabView(selection: $selectedTab) {
+            Text("Tab 1")
+                // 2. we need to modify that somewhere, which will ask SwiftUI to switch tabs
+                .onTapGesture {
+                    self.selectedTab = 1
+                }
+                .tabItem {
+                    Image(systemName: "star")
+                    Text("One")
+                }
+                // 4. we can attach a unique identifier to each view, and use that for the selected tab. These identifiers are called tags, and are attached using the tag() modifier
+                .tag(0)
+            
+            Text("Tab 2")
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("Two")
+            }
+            .tag(1)
+        }
     }
 }
 
