830ce1eaaf14dc6f7e27d5f2b38c69fb5b946e05
Jason Rueckert <phlacin@gmail.com>
2020-01-01 21:31:26 -0800

    Controlling image interpolation in SwiftUI

---
+++ 16 HotProspects/HotProspects/Fundamentals/ControllingImageInterpolation.swift
@@ -0,0 +1,21 @@
+//
+//  ControllingImageInterpolation.swift
+//  HotProspects
+//
+//  Created by Jason on 1/1/20.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import SwiftUI
+
+struct ControllingImageInterpolation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ControllingImageInterpolation_Previews: PreviewProvider {
+    static var previews: some View {
+        ControllingImageInterpolation()
+    }
+}
