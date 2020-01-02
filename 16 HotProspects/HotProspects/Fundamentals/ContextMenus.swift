86b8156de3a714c9e35555f077fdcbc9092a6c48
Jason Rueckert <phlacin@gmail.com>
2020-01-02 14:10:29 -0800

    Creating context menus finished

--- 16 HotProspects/HotProspects/Fundamentals/ContextMenus.swift
+++ 16 HotProspects/HotProspects/Fundamentals/ContextMenus.swift
@@ -9,8 +9,40 @@
 import SwiftUI
 
 struct ContextMenus: View {
+    @State private var backgroundColor = Color.red
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Hello, World!")
+                .padding()
+                .background(backgroundColor)
+
+            Text("Change Color")
+                .padding()
+                .contextMenu {
+                    Button(action: {
+                        self.backgroundColor = .red
+                    }) {
+                        Text("Red")
+                        Image(systemName: "checkmark.circle.fill")
+                        .foregroundColor(.red)
+                    }
+
+                    Button(action: {
+                        self.backgroundColor = .green
+                    }) {
+                        Text("Green")
+                        Image(systemName: "checkmark.circle.fill")
+                        .foregroundColor(.red)
+                    }
+
+                    Button(action: {
+                        self.backgroundColor = .blue
+                    }) {
+                        Text("Blue")
+                    }
+                }
+        }
     }
 }
 
