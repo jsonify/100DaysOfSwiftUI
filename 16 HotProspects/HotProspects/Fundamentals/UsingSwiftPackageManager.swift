fd115ec8d92e76c5cf4027bd492e592ccfcd3933
Jason Rueckert <phlacin@gmail.com>
2020-01-02 20:06:01 -0800

    Starting MovieTrailer app

---
+++ 16 HotProspects/HotProspects/Fundamentals/UsingSwiftPackageManager.swift
@@ -0,0 +1,21 @@
+//
+//  UsingSwiftPackageManager.swift
+//  HotProspects
+//
+//  Created by Jason on 1/2/20.
+//  Copyright © 2020 Jason. All rights reserved.
+//
+
+import SwiftUI
+
+struct UsingSwiftPackageManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UsingSwiftPackageManager_Previews: PreviewProvider {
+    static var previews: some View {
+        UsingSwiftPackageManager()
+    }
+}
